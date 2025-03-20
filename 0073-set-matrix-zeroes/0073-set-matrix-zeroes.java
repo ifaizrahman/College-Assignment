79630e23e7f48d5b38b0a4bb773fa2a6f292db32
ifaizrahman <129529223+ifaizrahman@users.noreply.github.com>
2025-03-20 11:21:12 +0530

    Time: 1 ms (75.52%), Space: 46 MB (16.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,40 +2,43 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-
-        int[][] new_mat= new int[m][n];
-        for(int i = 0; i< m; i++){
-        for (int j = 0; j < n; j++){
-            new_mat[i][j] = matrix[i][j];
-        }
-       }
+        int x=Integer.MIN_VALUE-5;
+        
+       
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    zeroes(i,j,new_mat);
+                    ones(i,j,matrix);
                 }
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                matrix[i][j]=new_mat[i][j];
+                if(matrix[i][j]== x){
+                   matrix[i][j]=0; 
+                }
             }
         }
 
     }
 
 
-    private static void zeroes(int i,int j, int[][] mat){
+    private static void ones(int i,int j, int[][] mat){
         int m=mat.length;
         int n=mat[0].length;
+        int x=Integer.MIN_VALUE-5;
         
         for(int k=0;k<n;k++){
-            mat[i][k]=0;
+            if(mat[i][k] !=0){
+            mat[i][k]=x;
+            }
         }
         for(int k=0;k<m;k++){
-            mat[k][j]=0;
+            if(mat[k][j] !=0){
+            mat[k][j]=x;
+            }
         }
     }
 
